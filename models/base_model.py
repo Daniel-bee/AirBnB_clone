9b8354dfc9d120a4595baa111b7987b7375cb3df
Daniel-bee <daniel123_86@live.com>
2022-01-28 15:45:07 +0300

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,10 @@ from datetime import datetime
 
 class BaseModel:
     """Simple Base Model class"""
-    id = str(uuid.uuid4())
-    created_at = datetime.today()
-    updated_at = datetime.today()
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def save(self):
         """Update the current datetime"""
